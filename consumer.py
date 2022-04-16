75e6251e82b17ed61da7e376d3db81c5d17f8fc6
marobertson11 <84516739+marobertson11@users.noreply.github.com>
2022-04-15 21:29:45 -0700

    Create consumer.py
    This is the regular version of the consumer that is provided. It also has the part where it would request the user describe which key they want to consume, but that part is currently commented out. That would be line 13 + 14, 48 with indented lines of 49-56, then lines 57 + 58.

---
+++ consumer.py
@@ -0,0 +1,64 @@
+#
+# Consume messages from Confluent Cloud
+# Using Confluent Python Client for Apache Kafka
+#
+# =============================================================================
+
+from confluent_kafka import Consumer
+import json
+import ccloud_lib
+
+
+if __name__ == '__main__':
+    #key = input("Enter the key you would like to consume: ")
+    #key = "b'" + key + "'"
+    # Read arguments and configurations and initialize
+    args = ccloud_lib.parse_args()
+    config_file = args.config_file
+    topic = args.topic
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Consumer instance
+    # 'auto.offset.reset=earliest' to start reading from the beginning of the
+    #   topic if no committed offsets exist
+    consumer_conf = ccloud_lib.pop_schema_registry_params_from_config(conf)
+    consumer_conf['group.id'] = 'python_example_group_1'
+    consumer_conf['auto.offset.reset'] = 'earliest'
+    consumer = Consumer(consumer_conf)
+
+    # Subscribe to topic
+    consumer.subscribe([topic])
+
+    # Process messages
+    total_count = 0
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # No message available within timeout.
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting for message or event/error in poll()")
+                continue
+            elif msg.error():
+                print('error: {}'.format(msg.error()))
+            else:
+                # Check for Kafka message
+                #if(str(msg.key()) == key):
+                record_key = msg.key()
+                record_value = msg.value()
+                data = json.loads(record_value)
+                count = data['count']
+                total_count += count
+                print("Consumed record with key {} and value {}, \
+                     and updated total count to {}"
+                     .format(record_key, record_value, total_count))
+                #else:
+                #    print("Record does not match key {}, record has key {}".format(key,msg.key()))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        print('\n',total_count)
+        consumer.close()
