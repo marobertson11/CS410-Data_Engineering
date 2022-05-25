fceaed55409ccb033f0eaa8a4e624e39f5154e72
marobertson11 <84516739+marobertson11@users.noreply.github.com>
2022-05-25 14:33:41 -0700

    Create run.sh
    Bash script that runs every morning to insert daily data to postgresql tables

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd ./examples/clients/cloud/python
+virtualenv ccloud-venv
+source ./ccloud-venv/bin/activate
+./topic_clean.py -f /home/mar32/examples/librdkafka.config -t datajob
+sleep 5
+./producer.py -f /home/mar32/examples/librdkafka.config -t datajob 
+sleep 5
+./consumer.py -f /home/mar32/examples/librdkafka.config -t datajob
+sleep 5
+./topic_clean.py -f /home/mar32/examples/librdkafka.config -t datajob
+
+./topic_clean.py -f /home/mar32/examples/librdkafka.config -t eventjob
+sleep 5
+./website-producer.py -f /home/mar32/examples/librdkafka.config -t eventjob
+sleep 5
+./website-consumer.py -f /home/mar32/examples/librdkafka.config -t eventjob
+sleep 5
+./topic_clean.py -f /home/mar32/examples/librdkafka.config -t eventjob
+STR=$(date +%Y-%m-%d)".json"
+FILE=/home/mar32/data/$STR
+gsutil cp $FILE gs://cs410data
+echo complete
